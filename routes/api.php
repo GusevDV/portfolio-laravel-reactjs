acaded80d6597cdff0c681f7377ed37bc6db76d4
Dmitry Gusev <dmivlagus@gmail.com>
2018-03-07 15:03:49 +0300

    create post controller and router

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/post', 'PostController@index');
+Route::get('/post/{post}', 'PostController@show');
+
+Route::post('/post', 'PostController@store');
+Route::put('/post/{id}', 'PostController@update');
+Route::delete('/post/{id}', 'PostController@destroy');
