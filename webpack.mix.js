007c50663b50607342808efec377e017025045bb
Dmitry Gusev <dmivlagus@gmail.com>
2018-03-17 22:41:19 +0300

    change dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
-mix.browserSync('dg.loc/public');
+mix.react('resources/assets/js/index.js', 'public/js').version();
+mix.sass('resources/assets/sass/app.scss', 'public/css').version();
+mix.browserSync('dg.loc');
